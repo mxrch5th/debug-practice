e9cd2f448118ff43b5352a29146ce372402a7801
mxrch5th <yooha4519@gamil.com>
2026-04-16 19:54:18 +0900

    by using git init method

---
+++ average_test.py
@@ -0,0 +1,9 @@
+# 문제: 리스트에 들어있는 숫자들의 평균을 출력해야 함
+numbers = [10, 20, 30, 40, 50]
+
+# 여기에 오류가 숨어있어! (힌트: 데이터 타입과 합계 계산)
+total = sum(numbers) 
+average = total / 5
+
+print("평균값은 다음과 같습니다:")
+print(average)
